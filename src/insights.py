22dcb9502fa5d25b6355dd4e40c77bffedb8a085
Arturo Vaine <arturo.vaine@gmail.com>
2022-08-04 17:21:46 -0300

    feat: req2, get_unique_job_types

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,6 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
     """Checks all different job types and returns a list of them
 
@@ -13,7 +16,11 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
+    jobs_list = read(path)
+    job_types = set()
+    for item in jobs_list:
+        job_types.add(item['job_type'])
+    return job_types
 
 
 def filter_by_job_type(jobs, job_type):
