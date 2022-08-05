bc86a15246b25dc709ee6e5f3f815e914dd5260c
Arturo Vaine <arturo.vaine@gmail.com>
2022-08-04 22:54:23 -0300

    feat: req12, test_sort_by_criteria

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,35 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+
+mock = [
+    {
+        "min_salary": 3000,
+        "max_salary": 7000,
+    },
+    {
+        "min_salary": 2000,
+        "max_salary": 8000,
+    },
+    {
+        "min_salary": 1000,
+        "max_salary": 9000,
+    },
+]
+
+sorted = [mock[2], mock[1], mock[0]]
 
 
 def test_sort_by_criteria():
-    pass
+    try:
+        sort_by(mock, "min_salary")
+        assert mock == sorted
+    except ValueError as err:
+        print(err)
+        raise
+
+    try:
+        sort_by(mock, "max_salary")
+        assert mock == sorted
+    except ValueError as err:
+        print(err)
+        raise
