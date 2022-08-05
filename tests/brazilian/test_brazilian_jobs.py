ae88554d670c2cc39ad62773f9f8ec60f67da1cd
Arturo Vaine <arturo.vaine@gmail.com>
2022-08-04 22:33:29 -0300

    feat: req11, test read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,14 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
+
+path = "tests/mocks/brazilians_jobs.csv"
+
+# dict_pt = {"titulo": "Maquinista", "salario": "2000", "tipo": "trainee"}
+dict_en = {"title": "Maquinista", "salary": "2000", "type": "trainee"}
 
 
 def test_brazilian_jobs():
-    pass
+    try:
+        dict = read_brazilian_file(path)
+        assert dict_en in dict
+    except ValueError:
+        raise
