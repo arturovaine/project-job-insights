16d35c692715f9149f555f7b450400bee0551393
Arturo Vaine <arturo.vaine@gmail.com>
2022-08-04 22:10:07 -0300

    feat: req10, test count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,30 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
+# from src.jobs import read
+# import pytest
+
+mock_py = [{}]
+mock_js = [{}]
+
+path = "src/jobs.csv"
+
+word_py = "python"
+word_js = "javascript"
+
+# python -> 1639
+# javascript -> 122
 
 
 def test_counter():
-    pass
+    try:
+        python_count = count_ocurrences(path, word_py)
+        assert python_count == 1639
+    except ValueError as err:
+        print(err)
+        raise
+
+    try:
+        javascript_count = count_ocurrences(path, word_js)
+        assert javascript_count == 122
+    except ValueError as err:
+        print(err)
+        raise
